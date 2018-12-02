7ee7aab8228c87866ac3f60ecd711d615e64563c
VadymRud <rudvadim@gmail.com>
2018-12-02 14:03:55 +0200

    Added Route and Vilage model

--- raspis_luka_server/rozklad/admin.py
+++ raspis_luka_server/rozklad/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from .models import Transport, Zupynka
+from .models import Transport, Zupynka, Village, RouteBus
 
 
 class TransportAdmin(admin.ModelAdmin):
@@ -25,3 +25,27 @@ class ZupynkaAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Zupynka, ZupynkaAdmin)
+
+
+class VillageAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (_('General information'), {'fields': ['name', 'zupynks' ]}),
+            (_('Active'), {'fields': ['active'], 'classes': ['collapse']}),
+        ]
+    readonly_fields = ('pk',)
+    list_display = ('__str__', 'name')
+
+
+admin.site.register(Village, VillageAdmin)
+
+
+class RouteBusAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (_('General information'), {'fields': ['name', 'from_village', 'to_village', 'across_village']}),
+            (_('Active'), {'fields': ['active'], 'classes': ['collapse']}),
+        ]
+    readonly_fields = ('pk',)
+    list_display = ('__str__', 'name')
+
+
+admin.site.register(RouteBus, RouteBusAdmin)
--- raspis_luka_server/rozklad/models.py
+++ raspis_luka_server/rozklad/models.py
@@ -25,4 +25,28 @@ class Zupynka(models.Model):
         verbose_name_plural = _('Zupynka Names')
 
 
+class Village(models.Model):
+    name = models.CharField(verbose_name = _('Name'), max_length=200)
+    active = models.BooleanField(verbose_name=_('Active'), default=True)
+    zupynks = models.ManyToManyField(Zupynka, verbose_name=_('Zupynka Names'))
+    #type = models.CharField(verbose_name = _('Type Vehicle'), max_length=200)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('Village Name')
+        verbose_name_plural = _('Village Names')
+
+
+class RouteBus(models.Model):
+    name = models.CharField(verbose_name = _('Name'), max_length=200)
+    active = models.BooleanField(verbose_name=_('Active'), default=True)
+    from_village = models.ForeignKey(Village, verbose_name=_('From village'), related_name='from_village')
+    to_village = models.ForeignKey(Village, verbose_name=_('From village'), related_name='to_village')
+    across_village = models.ManyToManyField(Village, verbose_name=_('From village'),related_name='across_village')
+    #type = models.CharField(verbose_name = _('Type Vehicle'), max_length=200)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('Route Bus Name')
+        verbose_name_plural = _('Route Bus Names')
 
